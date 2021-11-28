8f393d42063f968945f6e3bf3f22c83f1c46753f
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2021-11-28 12:56:50 +0500

    $Bubble Sort

--- Bubble Sort/BubbleSort.py
+++ Bubble Sort/BubbleSort.py
@@ -3,9 +3,13 @@ from timeit import Timer
 
 def bubbleSort(arr):
     for i in range(len(arr)):
-        for j in range(0, len(arr)-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j+1],arr[j] = arr[j],arr[j+1]
+        exchanges = True
+        if (exchanges is True):
+            exchanges = False
+            for j in range(0, len(arr)-i-1):
+                if arr[j] > arr[j+1]:
+                    exchanges = True
+                    arr[j+1],arr[j] = arr[j],arr[j+1]
     return arr
 
 if __name__ == "__main__":
@@ -13,7 +17,7 @@ if __name__ == "__main__":
     array = [54,26,93,17,77,31,44,55,20]
     print(array)
     print('After Sorting')
-    print('Sorted Array for Best Case is {}'.format(bubbleSort(array)))
+    print('Sorted Array is {}'.format(bubbleSort(array)))
 
     t1 = timeit.Timer("bubbleSort(" + str(array) + ")", "from __main__ import bubbleSort")
     print("Running Time of Bubble Sort :  ", t1.timeit(number=1000), "milliseconds")
---
+++ Shell Sort/Shell Sort.py
@@ -0,0 +1,26 @@
+import timeit
+from timeit import Timer
+
+def shellSort(arr):
+    size = len(arr)
+    interval = size//2
+    while interval > 0:
+        for i in range(interval, size):
+            temp = arr[i]
+            x = i
+            while x >= interval and arr[x-interval] > temp:
+                arr[x] = arr[x-interval]
+                x -= interval
+            arr[x] = temp
+        interval //= 2
+    return arr
+
+if __name__ == "__main__":
+    print('Before Sorting')
+    array = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print(array)
+    print('After Sorting')
+    print('Sorted Array is {}'.format(shellSort(array)))
+
+    t1 = timeit.Timer("shellSort(" + str(array) + ")", "from __main__ import shellSort")
+    print("Running Time of Shell Sort :  ", t1.timeit(number=1000), "milliseconds")
